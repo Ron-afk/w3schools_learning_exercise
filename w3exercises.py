f8d7d3270d749e66559551f9bd083e47b924cca9
rangxiao <xiaorang@student.ubc.ca>
2022-01-10 16:13:54 -0800

    Initial commit

---
+++ w3exercises.py
@@ -0,0 +1,49 @@
+# if 5 > 2:
+#     print("yeah!")
+#
+#
+# x = 5
+# y = "Hello, World!"
+#
+# print(y)
+
+# x = "awsome"
+#
+# def myfunc():
+#     x = "fantastic"
+#     print("Python is " + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
+#
+# print(type(x))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
